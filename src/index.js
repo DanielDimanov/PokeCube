18bfaad052b4b9fd122a2452145125840841afe3
Daniel Dimanov <i7461730@bournemouth.ac.uk>
2019-05-20 15:51:32 +0100

    Render initial application

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@ import * as serviceWorker from './serviceWorker';
 
 //Create main component
 const App = () => {
-    return <div> Welcome to PokeCube</div>;
+    return <div> Welcome to PokeCube </div>;
 }
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
+ReactDOM.render(<App/>, document.querySelector('#root'));
 serviceWorker.unregister();
