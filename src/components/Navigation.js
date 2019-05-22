6e3b76c491f04a6179381bd5a0e571755db1ecf0
Daniel Dimanov <i7461730@bournemouth.ac.uk>
2019-05-22 11:18:06 +0100

    Add the Navigation component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 
+//Import Components
+import Home from './Home';
+import Compare from './Compare';
+
 //Import Firebase
 import {providers,firebaseAppAuth} from './Firebase';
 
 import withFirebaseAuth from 'react-with-firebase-auth';
 import 'firebase/auth';
 
+//Import Router
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 //Import styling
 import './styles/Navigation.css';
+import Favourites from './Favourites';
 
 
 class Navigation extends Component {
-  constructor() {
-    super();
-  }
 
   render() {
     //Firebase session
@@ -27,18 +32,41 @@ class Navigation extends Component {
 
     return (
       <div className="Navigation">
-        <p>Dali </p>
-        {
-            user
-              ? <p>Hello, {user.displayName}</p>
-              : <p>Please sign in.</p>
-          }
+        <Router>
+        <div>
+            <nav>
+            <ul>
+                <li>
+                <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/compare">Compare</Link>
+                </li>
+               
+                {/* Floatted right, so inverse order */}
 
-          {
-            user
-              ? <button onClick={signOut}>Sign out</button>
-              : <button onClick={signInWithGoogle}>Sign in with Google</button>
-          }
+                <li className ="right-nav">
+                {
+                    user
+                    ? <button className="nav-listing" onClick={signOut}>Sign out</button>
+                    : <button className="nav-listing" onClick={signInWithGoogle}>Sign in with Google</button>
+                }
+                </li>
+                <li className="right-nav">
+                <Link to="/favourite">
+                    <img height="20px" alt="fav" src={process.env.PUBLIC_URL+"/icons/hp.png"}></img>
+                </Link>
+                </li>
+                
+            </ul>
+            </nav>
+
+            <Route path="/" exact component={Home} />
+            <Route path="/compare" exact component={Compare} />
+            <Route path="/favourite" exact component={Favourites} />
+        </div>
+        </Router>
+          
       </div>
     );
   }
